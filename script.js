103277c83c4e802d299dec2c0e0ec8b0ebb04a41
Riyaz <riyazshaik5027@gmail.com>
2025-10-25 13:30:09 +0530

    if else and === and ==

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let firstCard=10
 let secondCard=9
 let sum=firstCard+secondCard
+
+if(sum<21)
+{
+    console.log("Do you want to draw a new card")
+}
+else if(sum===21)
+{
+    console.log("WOHOOOOOO! you have got BlackJack")
+}
+else{
+    console.log("You are out of the game")
+}
