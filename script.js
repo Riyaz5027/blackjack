0fb1ab9f106d627119f7d4fa044c3ee029de175f
Riyaz <riyazshaik5027@gmail.com>
2025-10-25 15:34:39 +0530

    fixes

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-let firstCard=10
-let secondCard=9
-let sum=firstCard+secondCard
-let hasBlackJack=false
-let isAlive=true
+let firstCard = 10
+let secondCard = 9
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if(sum<21)
-{
-    console.log("Do you want to draw a new card")
-}
-else if(sum===21)
-{
-    hasBlackJack=true
-    console.log("WOHOOOOOO! you have got BlackJack")
-}
-else{
-    isAlive=false
-    console.log("You are out of the game")
-}
-
-
-if(hasBlackJack)
-{
-    console.log("You have ge")
+function startGame() {
+    document.getElementById("sum-el").textContent="Sum: "+sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card"
+    }
+    else if (sum === 21) {
+        hasBlackJack = true
+        message = "WOHOOOOOO! you have got BlackJack"
+    }
+    else {
+        isAlive = false
+        message = "You are out of the game"
+    }
+    document.getElementById("message-el").textContent=message
+    console.log(message)
 }
