b0e1813800e6e6e088383d2cc07db62d6e5af920
Riyaz <riyazshaik5027@gmail.com>
2025-10-25 17:08:55 +0530

    Arrays and for loop

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let firstCard = 10
-let secondCard = 9
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let cards=[firstCard,secondCard]
 
 //alternative to document.getElementByID("sum-el") is
 //document.querySelector("#sum-el")
 
+
+
 function startGame() {
-    document.getElementById("cards-el").textContent="Card: " + " " + firstCard + " " + secondCard
+    renderGame()
+}
+function renderGame() {
+    for(let i=0;i<cards.length;i++)
+    {
+        document.getElementById("cards-el").textContent+=cards[i]+" "
+    }
     document.getElementById("sum-el").textContent="Sum: "+sum
     if (sum <= 20) {
         message = "Do you want to draw a new card"
@@ -24,4 +33,10 @@ function startGame() {
     }
     document.getElementById("message-el").textContent=message
     console.log(message)
+}
+
+function newCard(){
+    let card = 7
+    sum += card
+    renderGame()
 }
